94daa7f295b6630c65ce09d33867e9f6def613b3
han250226 <ej.han@fixspeed.co.kr>
2025-02-26 13:31:06 +0900

    폴더이름변경

--- src/main/java/com/example/admin/controller/web/DashBoardController.java
+++ src/main/java/com/example/admin/controller/web/DashBoardController.java
@@ -5,10 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminController {
+public class DashBoardController {
 
     @GetMapping("/dashboard")
     public String dashboard(Model model) {
-        return "dashboard"; // 타임리프 템플릿
+        return "dashBoard";
     }
 }
